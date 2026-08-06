d53cfa03154ce75747905f267113925c24bba368
ashukashi <kashinath@kashinaths-Air.lan>
2026-08-06 10:49:45 +0530

    fix(cache): Bump browser trace cache version key (dbg_v2_cache_) to purge old cached trace payloads with hardcoded white colors

--- static/js/debugger.js
+++ static/js/debugger.js
@@ -395,7 +395,7 @@ function getCacheKey(lang, codeStr, bps) {
     hash = ((hash << 5) - hash) + str.charCodeAt(i);
     hash |= 0;
   }
-  return `dbg_cache_${hash}`;
+  return `dbg_v2_cache_${hash}`;
 }
 
 function getTraceFromCache(lang, codeStr, bps) {
